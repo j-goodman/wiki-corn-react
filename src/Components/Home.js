6954e5c755343ae3a3eb4f9503d2a717ce6c8087
j-goodman <johngoodman@pursuit.org>
2023-08-14 20:47:56 -0400

    Restore featured article.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,7 +8,7 @@ function Home() {
     const [article, setArticle] = useState({});
     let navigate = useNavigate();
     const API = process.env.REACT_APP_API_URL;
-    const featuredTitle = "Dr Pepper"
+    const featuredTitle = "Zea mays"
 
     useEffect(() => {
       axios
@@ -23,7 +23,7 @@ function Home() {
   
     return (
       <div className="Article">
-        <h2>Welcome to Wiki Corn!</h2>
+        <h2>Welcome to Wiki Corn</h2>
         <h3><b className="greyout">Featured Article:</b>&nbsp;<Link to={`/articles/${article.title}`}>{article.title}</Link></h3>
         <div dangerouslySetInnerHTML={{ __html: article.body }} />
       </div>
